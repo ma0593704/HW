a01522e5a1d02421ced743eb3742a6bc7f764450
ghif <gbgu>
2021-03-21 19:34:45 +0300

    ty

--- src/hwork2/HW1.java
+++ src/hwork2/HW1.java
@@ -1,5 +1,19 @@
 package hwork2;
 
+import java.util.Scanner;
+
 public class HW1 {
-    int j;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество правильных ответов");
+        int count = in.nextInt();
+        if (count < 0) System.out.println("Неккоректно введены данные");
+        else if (count <= 39) System.out.println("Попробуйте в следующий раз");
+        else if (count <= 59) System.out.println("Удовлетворительно");
+        else if (count <= 89) System.out.println("Хорошо");
+        else if (count <= 100) System.out.println("Отлично");
+        else System.out.println("Больше 100? Серьезно?");
+
+
+    }
 }
