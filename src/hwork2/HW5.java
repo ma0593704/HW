9e56dd18bea38a7e0d119332bb8a80bc3364f6b2
ghif <gbgu>
2021-03-22 11:41:12 +0300

    ty

--- src/hwork2/HW5.java
+++ src/hwork2/HW5.java
@@ -5,12 +5,11 @@ public class HW5 {
         double i = Math.random() * 1000;
         int a = (int) i;
         System.out.println("Случайное число " + a);
-        if (a < 10) return;
-        else if (a > 500) return;
-        else {
+        if (a > 10 && a < 500) {
             if (a > 200) System.out.println("Случайное число " + a + " не содержится в интервале (25;200)");
             else if (a < 25) System.out.println("Случайное число " + a + " не содержится в интервале (25;200)");
             else System.out.println("Случайное число " + a + " содержится в интервале (25;200)");
+
         }
     }
 }
