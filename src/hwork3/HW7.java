36322769a0bb17c1d15a25e42fda7f6c0a9df47a
ghif <gbgu>
2021-03-26 12:21:15 +0300

    ty

--- src/hwork3/HW7.java
+++ src/hwork3/HW7.java
@@ -6,7 +6,7 @@ public class HW7 {
     public static void main(String[] args) {
         int[] m1 = new int[11];
         int count = 0, count1 = 0, count2 = 0;
-        int q=Integer.MAX_VALUE,w=Integer.MAX_VALUE,e=Integer.MAX_VALUE;
+        int q = 0, w = 0, e = 0;
         for (int i = 0; i < m1.length; i++) {
             m1[i] += Math.random() * (-3) + 1.5;
             if (m1[i] > 0) {
@@ -25,8 +25,8 @@ public class HW7 {
         if (count > count1 && count > count2) System.out.println(count + " " + q);
         else if (count1 > count && count1 > count2) System.out.println(count1 + " " + w);
         else if (count2 > count && count2 > count1) System.out.println(count2 + " " + e);
-        else if (count1 == count&&count==count1) System.out.println(count1 +  " это " + q + " и " + w);
-        else if (count2 == count&&count==count2) System.out.println(count2 +  " это " + q + " и " + e);
+        else if (count1 == count && count == count1) System.out.println(count1 + " это " + q + " и " + w);
+        else if (count2 == count && count == count2) System.out.println(count2 + " это " + q + " и " + e);
         System.out.println(Arrays.toString(m1));
     }
 }
